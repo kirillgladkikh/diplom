d7a8dfbd218d26a339bf4e55343ee8edf9598c36
Kirill Gladkikh <k.gladkikh@mail.ru>
2026-04-17 17:05:49 +0300

    exporter.py clean

--- src/exporter.py
+++ src/exporter.py
@@ -34,28 +34,3 @@ class CSVExporter:
         print(f"Данные успешно сохранены в {full_path}")
         print(f"Всего записей: {len(products)}")
         return True
-
-
-# import csv
-# # from .product import Product
-#
-# class CSVExporter:
-#     def __init__(self, filename):
-#         self.filename = filename
-#
-#     def export(self, products):
-#         with open(self.filename, 'w', newline='', encoding='utf-8') as csvfile:
-#             fieldnames = [
-#                 "Ссылка на продукт",
-#                 "Наименование",
-#                 "Цена",
-#                 "Рейтинг пользователей",
-#                 "Описание продукта",
-#                 "Инструкция по применению",
-#                 "Страна-производитель"
-#             ]
-#             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#             writer.writeheader()
-#             for product in products:
-#                 writer.writerow(product.to_dict())
-#         print(f"Данные успешно сохранены в {self.filename}")
