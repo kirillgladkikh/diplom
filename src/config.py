336df24e1493f7dfd63a1eebe67021944e45bc64
Kirill Gladkikh <k.gladkikh@mail.ru>
2026-04-17 17:19:57 +0300

    src/config.py clean selectors

--- src/config.py
+++ src/config.py
@@ -23,51 +23,18 @@ SELECTORS_PLP = {
 
 # СЕЛЕКТОРЫ ДЛЯ СТРАНИЦЫ ТОВАРА (PDP)
 SELECTORS_PDP = {
-    # Наименование продукта
-    "product_name": ["h1"],
-
     # Цена
     "product_price": [
         "._ga-price_1dj1y_114",
         "[itemprop='price']",
         "._ga-pdp-price-guest__price_1mmpu_157 ._ga-price_1dj1y_114",
         "[class*='_ga-price']",
-        # "[itemprop='price']", РАБОТАЕТ
-        # "._ga-price_1dj1y_114", РАБОТАЕТ
     ],
 
-    # Рейтинг (используем частичное совпадение, игнорируя динамический хэш)
-    "product_rating": [
-        "[itemprop='ratingValue']",  # Самый надёжный
-        "._ga-review-score-point__numeral_y1ia3_15",  # Точный класс
-        "[class*='_ga-review-score-point__numeral']",  # Частичное совпадение
-        "[class*='rating-value']",  # Альтернативный вариант
-        # "[itemprop='ratingValue']",  # Самый надёжный
-        # "._ga-review-score-point__numeral_y1ia3_15",  # Точный класс
-        # "[class*='_ga-review-score-point__numeral']",  # Частичное совпадение
-        # # "[class*='_ga-review-score-point__numeral']",  # Любой класс, содержащий эту подстроку
-        # # "[class*='rating-value']",  # Альтернативный вариант
-        # # "[itemprop='ratingValue']",  # Schema.org атрибут
-    ],
-
-    # Описание (из блока wysiwyg)
+    # Описание продукта (из блока wysiwyg)
     "product_description": [
         "._ga-pdp-wysiwyg_rmnt6_55",
         "[itemprop='description']",
-    ],
-
-    # Инструкция по применению
-    "product_instructions": [
-        "//section[.//h2[contains(text(), 'Применение')]]//div[contains(@class, '_ga-pdp-wysiwyg_rmnt6_55')]",
-        "//div[contains(@class, 'ga-accordion-item')][.//h2[contains(text(), 'Применение')]]//div[contains(@class, '_ga-pdp-wysiwyg')]",
-    ],
-
-    # Страна-производитель
-    "product_country": [
-        "//div[contains(@class, 'wysiwyg') and contains(text(), 'страна происхождения')]",
-        "//div[contains(@class, '_ga-pdp-wysiwyg')]//*[contains(text(), 'страна происхождения')]/following-sibling::text()[1]",
-        # "//section[.//h2[contains(text(), 'Дополнительная информация')]]//div[contains(@class, '_ga-pdp-wysiwyg_rmnt6_55')]",
-        # "//div[contains(@class, 'ga-accordion-item')][.//h2[contains(text(), 'Дополнительная информация')]]//div[contains(@class, '_ga-pdp-wysiwyg')]",
     ],
 }
 
